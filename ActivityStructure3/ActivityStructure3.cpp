ec2307cb6f80cceb819d6b94b452c59794b43b00
BintangDanendra <bintang.danendra140704@gmail.com>
2023-04-05 11:46:42 +0700

    first commite

--- ActivityStructure3/ActivityStructure3.cpp
+++ ActivityStructure3/ActivityStructure3.cpp
@@ -1,6 +1,45 @@
 #include <iostream>
+using namespace std;
 
-int main()
+struct DetailAlamat
 {
-    std::cout << "Hello World!\n";
-} 
+    char Desa[20];
+    char Kota[20];
+};
+
+struct mahasiswa
+{
+    char nim[12];
+    char nama[20];
+    DetailAlamat alamat;
+    int umur;
+};
+
+int main() {
+    mahasiswa mhs[3];
+
+    for (int i = 0; i < 3; i++) {
+        cout << "Masukkan NIM : ";
+        cin.getline(mhs[i].nim,12);
+        cout << "Masukkan Umur : ";
+        cin.ignore(1, '\n');
+        cout << "Masukkan Nama : ";
+        cin.getline(mhs[i].nama, 20);
+        cout << "Alamatn : ";
+        cout << "\n\tMasukkan Desa : ";
+        cin.getline(mhs[i].alamat.Desa, 20);
+        cout << "\tMasukkan Kota : ";
+        cin.getline(mhs[i].alamat.Kota, 20);
+
+    }
+
+    for (int i = 0; i < 3; i++) {
+        cout << "\n\nNIM : " << mhs[i].nim;
+        cout << "\nUmur : " << mhs[i].umur;
+        cout << "\nNama : " << mhs[i].nama;
+        cout << "\nAlamat : ";
+        cout << "\nDesa : " << mhs[i].alamat.Desa;
+        cout << "\nKota : " << mhs[i].alamat.Kota;
+    }
+
+}
